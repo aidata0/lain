f421963243819744c241ec9ec8ca2566c83199c7
aidata0 <104957044+aidata0@users.noreply.github.com>
2024-11-11 12:23:16 +0800

    Create layout.js

---
+++ layout.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'lain: neural network',
+  description: 'Layer connections across consciousness',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
