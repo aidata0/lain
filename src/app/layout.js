b8d95893bad6ccf4e87c4089adcdac19f49556e8
aidata0 <104957044+aidata0@users.noreply.github.com>
2024-11-11 12:36:15 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,9 +3,6 @@ import './globals.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <title>WIRED_PROTOCOL</title>
-      </head>
       <body>{children}</body>
     </html>
   )
